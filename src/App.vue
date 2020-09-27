15f463eddae3eb848aec14a939b4f30d3e9142bf
Simen Iversen <simen.m.iversen@gmail.com>
2020-09-27 18:50:21 +0200

    Final edit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
         <div id="app">
+          <h1>Recipes</h1>
             <div v-for="recipe in recipes" :key="recipe.title + recipe.ingredients" class="card">
                 <RecipeComponent        v-bind:title="recipe.title"
                                         v-bind:website="recipe.href"
@@ -58,12 +59,11 @@ body{
 }
 
 .card{
-  position: relative;
   background: rgb(231,255,160);
   background: linear-gradient(43deg, rgba(231,255,160,1) 0%, rgba(148,255,238,1) 48%, rgba(204,255,190,1) 81%, rgba(199,255,180,1) 100%);
   display: inline-block;
   width: 200px;
-  height: 320px;
+  height: 370px;
   border-radius: 10px;
   vertical-align: top;
   margin: 10px;
--- src/components/RecipeComponent.vue
+++ src/components/RecipeComponent.vue
@@ -1,9 +1,12 @@
 <template>
         <div class="card-info">
           <img :src="recipeImg" class="img"/>
-          <p>Title:{{title}} </p>
-          <p>Ingredients: {{ingredients}}</p>
-          <a class="link" rel="noreferrer noopener" :href="website" target="_blank">Click Here for recipe</a> 
+          <h2>{{title}}</h2>
+          <div class="ingredient-box">
+            <p>{{ingredients}}</p>
+            <a class="link" rel="noreferrer noopener" :href="website" target="_blank">Click Here for recipe</a>
+          </div>
+           
         </div>
 </template>
 
@@ -20,9 +23,32 @@ export default {
 }
 .img{
   border-radius: 15px;
+  box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.611);
+}
+.ingredient-box{
+  position: relative;
+  background: rgba(48, 48, 48, 0.864);
+  color: white;
+  height: 150px;
+  padding: 20px;
+  border-radius: 15px;
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.611);
+}
+p{
+  margin: 0;
+}
+.link{
+  color: white;
+  position: absolute;
+  bottom: 15px;
+  left: 8px;
 }
 .link:hover{
   background:black;
   color: white;
+}
+h2{
+  font-size: 16px;
+  height: 30px;
 }
 </style>
